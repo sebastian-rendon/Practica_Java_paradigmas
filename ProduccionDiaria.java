4d1ecf9fcfe0842446847d8d4439883b73286151
Sebastian Rendon <srendongrisales@gmail.com>
2026-09-13 15:53:27 -0500

    Constructor finalizado

--- ProduccionDiaria.java
+++ ProduccionDiaria.java
@@ -23,3 +23,117 @@
 //Al finalizar, el sistema deberá mostrar un reporte con las líneas existentes, producción total por línea, productos críticos, productos que superaron la meta, unidades defectuosas, costo total de producción, pérdidas económicas, producto con mayor pérdida y línea con mayor cantidad producida.
 
 //La solución deberá desarrollarse principalmente utilizando programación funcional en Java, evitando ciclos tradicionales para las operaciones de análisis y transformación cuando puedan resolverse utilizando Streams.
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
+class RegistroProduccion {
+    private final int codigoProducto;
+    private final String nombreProducto;
+    private final String lineaProduccion;
+    private int cantidadProducida;
+    private int cantidadDefectuosa;
+    private double costoUnitario;
+    private int minutosUtilizados;
+    private double kilogramosMateriaPrima;
+    private int metaProduccion;
+
+    public RegistroProduccion(int codigoProducto, String nombreProducto, String lineaProduccion,
+                              int cantidadProducida, int cantidadDefectuosa, double costoUnitario,
+                              int minutosUtilizados, double kilogramosMateriaPrima, int metaProduccion) {
+
+        this.codigoProducto = codigoProducto;
+        this.nombreProducto = nombreProducto;
+        this.lineaProduccion = lineaProduccion;
+        this.cantidadProducida = cantidadProducida;
+        this.cantidadDefectuosa = cantidadDefectuosa;
+        this.costoUnitario = costoUnitario;
+        this.minutosUtilizados = minutosUtilizados;
+        this.kilogramosMateriaPrima = kilogramosMateriaPrima;
+        this.metaProduccion = metaProduccion;
+    } 
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public String getLineaProduccion() {
+        return lineaProduccion;
+    }
+
+    public int getCantidadProducida() {
+        return cantidadProducida;
+    }
+
+    public int getCantidadDefectuosa() {
+        return cantidadDefectuosa;
+    }
+
+    public double getCostoUnitario() {
+        return costoUnitario;
+    }
+
+    public int getMinutosUtilizados() {
+        return minutosUtilizados;
+    }
+
+    public double getKilogramosMateriaPrima() {
+        return kilogramosMateriaPrima;
+    }
+
+    public int getMetaProduccion() {
+        return metaProduccion;
+    }
+
+    public void setCantidadProducida(int cantidadProducida) {
+        this.cantidadProducida = cantidadProducida;
+    }
+
+    public void setCantidadDefectuosa(int cantidadDefectuosa) {
+        this.cantidadDefectuosa = cantidadDefectuosa;
+    }
+
+    public void setCostoUnitario(double costoUnitario) {
+        this.costoUnitario = costoUnitario;
+    }
+
+    public void setMinutosUtilizados(int minutosUtilizados) {
+        this.minutosUtilizados = minutosUtilizados;
+    }
+
+    public void setKilogramosMateriaPrima(double kilogramosMateriaPrima) {
+        this.kilogramosMateriaPrima = kilogramosMateriaPrima;
+    }
+
+    public void setMetaProduccion(int metaProduccion) {
+        this.metaProduccion = metaProduccion;
+    }
+
+    @Override
+    public String toString() {
+        return "registroProduccion{" +
+                "codigoProducto=" + codigoProducto +
+                ", nombreProducto='" + nombreProducto + '\'' +
+                ", lineaProduccion='" + lineaProduccion + '\'' +
+                ", cantidadProducida=" + cantidadProducida +
+                ", cantidadDefectuosa=" + cantidadDefectuosa +
+                ", costoUnitario=" + costoUnitario +
+                ", minutosUtilizados=" + minutosUtilizados +
+                ", kilogramosMateriaPrima=" + kilogramosMateriaPrima +
+                ", metaProduccion=" + metaProduccion +
+                '}';
+    }
+}
+
